c654c694881508cbd27b379043e4c2da9b2c48bd
xDamneDx <xdamnedx@gmail.com>
2022-04-27 15:29:34 +0300

    Added dropdown component to the UI Library

--- src/index.js
+++ src/index.js
@@ -1,4 +1,11 @@
 import Tooltip from "./ninja-ui/tooltip";
+import Dropdown from "./ninja-ui/dropdown";
 
 const tooltip = new Tooltip(document.querySelector(".tooltip"));
+const dropdowns = document.querySelectorAll(".dropdown");
+
 tooltip.init();
+dropdowns.forEach((dropdown) => {
+  const instance = new Dropdown(dropdown);
+  instance.init();
+});
