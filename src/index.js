649c691c5868818daf1b7d4b2c7110110059814c
xDamneDx <xdamnedx@gmail.com>
2022-04-27 18:26:43 +0300

    Added snackbar component to the UI Library

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 import Tooltip from "./ninja-ui/tooltip";
 import Dropdown from "./ninja-ui/dropdown";
 import Tabs from "./ninja-ui/tabs";
+import Snackbar from "./ninja-ui/snackbar";
 
 const tooltip = new Tooltip(document.querySelector(".tooltip"));
 const dropdowns = document.querySelectorAll(".dropdown");
 const tabs = new Tabs(document.querySelector(".tabs"));
+const button = document.querySelector(".snackbar-trigger");
 
 tooltip.init();
 dropdowns.forEach((dropdown) => {
@@ -12,3 +14,10 @@ dropdowns.forEach((dropdown) => {
   instance.init();
 });
 tabs.init();
+
+const snackbar = new Snackbar();
+snackbar.init();
+
+button.addEventListener("click", () => {
+  snackbar.show("You clicked me :)");
+});
